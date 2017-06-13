25fe901472713c01e67766d2c496d59ee8bac026
Subramanyam R <subramanyam@Subramanyams-MacBook-Pro.local>
2017-06-13 14:06:15 +0530

    Added helper function to generate color mapping and tests for same

---
+++ tests/testthat/test-plot_helper.R
@@ -0,0 +1,29 @@
+library(testthat)
+library(timelineR)
+
+context("Testing generate_color_mapping function")
+
+test_that("All columns have same set of values", {
+  test_df = data.frame(timestamp = as.POSIXct(c("2017-01-01","2017-01-02", "2017-01-03")), 
+                       col_1 = c("value1", "value2", "value3"), 
+                       col_2 = c("value1", "value2", "value3"))
+  default_color_mapping = c("value1" = "green", "value2" = "red", "value3" = "green")
+  
+  expected_output = structure(list(col_1 = structure(c("green", "red", "green"), .Names = c("value1", "value2", "value3")),                                      col_2 = structure(c("green", "red", "green"), .Names = c("value1", "value2", "value3"))),                               .Names = c("col_1","col_2"))
+  
+  expect_equal(generate_color_mapping(test_df, default_color_mapping), expected_output)
+  # plot_timeline(test_df, color_mapping =  expected_output)
+  
+})
+
+test_that("All columns do not contain all values", {
+  test_df = data.frame(timestamp = as.POSIXct(c("2017-01-01","2017-01-02", "2017-01-03")), 
+                       col_1 = c("value1"), 
+                       col_2 = c("value1", "value2", "value3"))
+  default_color_mapping = c("value1" = "green", "value2" = "red", "value3" = "green")
+  
+  expected_output = structure(list(col_1 = structure(c("green"), .Names = c("value1")),                                                                          col_2 = structure(c("green", "red", "green"), .Names = c("value1", "value2", "value3"))),                               .Names = c("col_1","col_2"))
+  
+  expect_equal(generate_color_mapping(test_df, default_color_mapping), expected_output)
+})
+
